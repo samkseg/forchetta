7a135d26a33cdb27ca5e15224ffafd3abb2e0f1e
Ken <ken.segawa@iths.se>
2023-09-27 12:19:17 +0200

    Front page with basic css, a first component and rendered list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
+    <div class="full-page-layout">
+        <header class="header">
+            <h1>{{ headerText }}</h1>
+            <p>{{ paragraphText }}</p>
+        </header>
 
+        <nav class="menu">
+            <a href="">Start</a>
+            <a href="">Category</a>
+            <a href="">Recept</a>
+            <a><Button></Button></a>
+        </nav>
 
-
+        <main class="main">
+            <ul>
+                <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+            </ul>
+        </main>
+    </div>
 </template>
 
 <script>
+import Button from "./Button.vue"
 
-
-
+export default {
+    components: {
+        Button
+    },
+    data() {
+        return {
+            headerText: "Forchetta Receptbok",
+            paragraphText: "Nånting om recept",
+            list: ["Recept 1", "Recept 2", "Recept 3"]
+        }
+    }
+}
 
 </script>
 
 <style>
-
-
-
-
 </style>
---
+++ src/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="increment()">
+        You clicked me {{ count }} times.
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() { 
+            this.count++;
+        }
+    }
+}
+</script>
