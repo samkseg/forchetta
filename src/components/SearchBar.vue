3ef0a4277ea6b6e5924e9d670ca2d37cfecdedf8
Ken <ken.segawa@iths.se>
2023-10-02 02:22:47 +0200

    Updated SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,20 +12,10 @@ export default {
             searchText: ""
         }
     },
-    created() {
-        this.initFormState();
-    },
     methods: {
         searchList: function () {
             if (this.searchText != null & this.searchText != "") {
                 this.$router.push({ name: "Search", params: { search: this.searchText } })
-                sessionStorage.setItem("searchText", this.searchText);
-            }
-        },
-        initFormState: function () {
-            const searchText = sessionStorage.getItem("searchText") || "";
-            if (searchText != null) {
-                this.searchText = searchText;
             }
         }
     }
