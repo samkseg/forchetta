495181395ad4ea8cb977c591ffc4e0e152a551af
Ken <ken.segawa@iths.se>
2023-10-02 02:46:35 +0200

    Updated SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="search-bar">
-        <input class="search-field" type="text" v-model="searchText" @keypress.enter="searchList"
-            v-on:change="saveFormState" placeholder=" Search Recipe...">
+        <input class="search-field" type="text" v-model="searchText" @keypress.enter="searchList" placeholder=" Search Recipe...">
     </div>
 </template>
 
@@ -16,6 +15,7 @@ export default {
         searchList: function () {
             if (this.searchText != null & this.searchText != "") {
                 this.$router.push({ name: "Search", params: { search: this.searchText } })
+                this.searchText = "";
             }
         }
     }
